44bf85e8e6a90b4e534a3ae4491fe574cbc5cb68
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-06 21:58:42 +0200

    AI Zombie States / Threath Definitions

--- Assets/_TheyKeepComing/Scripts/AI/AIState.cs
+++ Assets/_TheyKeepComing/Scripts/AI/AIState.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public abstract class AIState : MonoBehaviour {
 
-    public void SetStateMachine(AIStateMachine stateMachine) { stateMachine = _stateMachine; }
+    public virtual void SetStateMachine(AIStateMachine stateMachine) { stateMachine = _stateMachine; }
 
     public virtual void OnEnterState()          {}
     public virtual void OnExitState()           {}
-    public virtual void OnAnimatorUpdated()     {}
+
     public virtual void OnAnimatorIKUpdated()   {}
     public virtual void OnTriggerEvent(AITriggerEventType eventType, Collider other) {}
     public virtual void OnDestinationReached (bool isReached) {}
@@ -17,5 +17,33 @@ public abstract class AIState : MonoBehaviour {
     public abstract AIStateType OnUpdate();
 
     protected AIStateMachine _stateMachine;
+
+    public virtual void OnAnimatorUpdated()
+    {
+        if (_stateMachine.useRootPosition)
+            _stateMachine.navAgent.velocity = _stateMachine.animator.deltaPosition / Time.deltaTime;
+
+        if (_stateMachine.useRootRotation)
+            _stateMachine.transform.rotation = _stateMachine.animator.rootRotation;
+    }
+
+    public static void ConvertSphereColliderToWorldSpace (SphereCollider col, out Vector3 pos, out float radius)
+    {
+        pos = Vector3.zero;
+        radius = 0.0f;
+
+        if (col == null)
+            return;
+
+        pos = col.transform.position;
+        pos.x += col.center.x * col.transform.lossyScale.x;
+        pos.y += col.center.y * col.transform.lossyScale.y;
+        pos.z += col.center.z * col.transform.lossyScale.z;
+
+        radius = Mathf.Max(col.radius * col.transform.lossyScale.x,
+                           col.radius * col.transform.lossyScale.y);
+
+        radius = Mathf.Max(radius, col.radius * col.transform.lossyScale.z);
+    }
 
 }
