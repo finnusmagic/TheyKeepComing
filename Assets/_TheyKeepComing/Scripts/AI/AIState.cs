c21a647bb383852df25259b9df1cffe352856d08
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-06 18:09:18 +0200

    AI State Machine Setup

---
+++ Assets/_TheyKeepComing/Scripts/AI/AIState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIState : MonoBehaviour {
+
+    public void SetStateMachine(AIStateMachine stateMachine) { stateMachine = _stateMachine; }
+
+    public virtual void OnEnterState()          {}
+    public virtual void OnExitState()           {}
+    public virtual void OnAnimatorUpdated()     {}
+    public virtual void OnAnimatorIKUpdated()   {}
+    public virtual void OnTriggerEvent(AITriggerEventType eventType, Collider other) {}
+    public virtual void OnDestinationReached (bool isReached) {}
+
+    public abstract AIStateType GetStateType();
+    public abstract AIStateType OnUpdate();
+
+    protected AIStateMachine _stateMachine;
+
+}
