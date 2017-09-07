4bcd29cd027f6f9f18a0ecc16f7c11e60870bf6b
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-07 15:17:49 +0200

    Attack System / Animations Implementation

--- Assets/_TheyKeepComing/Scripts/AI/AI Zombie States/AIZombieState_Pursuit1.cs
+++ Assets/_TheyKeepComing/Scripts/AI/AI Zombie States/AIZombieState_Pursuit1.cs
@@ -9,6 +9,8 @@ using UnityEngine.AI;
 public class AIZombieState_Pursuit1 : AIZombieState
 {
     [SerializeField] [Range(0, 10)] private float _speed = 1.0f;
+    [SerializeField] [Range(0.0f, 1.0f)] float _lookAtWeight = 0.7f;
+    [SerializeField] [Range(0.0f, 90.0f)] float _lookAtAngleThreshold = 15.0f;
     [SerializeField] private float _slerpSpeed = 5.0f;
     [SerializeField] private float _repathDistanceMultiplier = 0.035f;
     [SerializeField] private float _repathVisualMinDuration = 0.05f;
@@ -17,9 +19,11 @@ public class AIZombieState_Pursuit1 : AIZombieState
     [SerializeField] private float _repathAudioMaxDuration = 5.0f;
     [SerializeField] private float _maxDuration = 40.0f;
 
+
     // Private Fields
     private float _timer = 0.0f;
     private float _repathTimer = 0.0f;
+    private float _currentLookAtWeight = 0.0f;
 
     // Mandatory Overrides
     public override AIStateType GetStateType() { return AIStateType.Pursuit; }
@@ -48,6 +52,7 @@ public class AIZombieState_Pursuit1 : AIZombieState
         _zombieStateMachine.navAgent.SetDestination(_zombieStateMachine.targetPosition);
         _zombieStateMachine.navAgent.Resume();
 
+        _currentLookAtWeight = 0.0f;
     }
 
     // ---------------------------------------------------------------------
@@ -240,6 +245,25 @@ public class AIZombieState_Pursuit1 : AIZombieState
         // Default
         return AIStateType.Pursuit;
     }
+    // -----------------------------------------------------------------------
+    // Name	:	OnAnimatorIKUpdated
+    // Desc	:	Override IK Goals
+    // -----------------------------------------------------------------------
+    public override void OnAnimatorIKUpdated()
+    {
+        if (_zombieStateMachine == null)
+            return;
 
-
+        if (Vector3.Angle(_zombieStateMachine.transform.forward, _zombieStateMachine.targetPosition - _zombieStateMachine.transform.position) < _lookAtAngleThreshold)
+        {
+            _zombieStateMachine.animator.SetLookAtPosition(_zombieStateMachine.targetPosition + Vector3.up);
+            _currentLookAtWeight = Mathf.Lerp(_currentLookAtWeight, _lookAtWeight, Time.deltaTime);
+            _zombieStateMachine.animator.SetLookAtWeight(_currentLookAtWeight);
+        }
+        else
+        {
+            _currentLookAtWeight = Mathf.Lerp(_currentLookAtWeight, 0.0f, Time.deltaTime);
+            _zombieStateMachine.animator.SetLookAtWeight(_currentLookAtWeight);
+        }
+    }
 }
